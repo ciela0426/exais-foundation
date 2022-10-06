ea46a986ebe3fe9338db537c00bd1a5f111fde0d
orbit <ciela0426@naver.com>
2022-10-06 15:13:47 +0900

    [feat] 페이지 이동시 scrollToTop

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,18 +5,13 @@ import NormalButton from "../components/NormalButton.js";
 
 const Main = () => {
     
-    
     return (
         <div className="container">
             {/* main box 1 */}
             <div className="main_box1">
                 <div className="main_box1_inner">
-                    <p>
-                        {t('main_title')}
-                    </p>
-                    <p>
-                        {t('main_description')}
-                    </p>
+                    <p className="title">{t('main_title')}</p>
+                    <p className="description">{t('main_description')}</p>
                     <div className="arrow_button">
                         <p>{t('white_paper')}</p>
                         <div className="button_arrow"></div>
@@ -26,54 +21,38 @@ const Main = () => {
             {/* main box 2 */}
             <div className="main_box2">
                 <div className="main_box2_inner">
-                    <div className="content_box">
-                        <p
-                            className="title"
-                            data-aos="fade-up"
-                            data-aos-duration="1000"
-                        >
-                            {t('main_box1_title')}
-                        </p>
-                        <p
-                            className="description"
-                            data-aos="fade-up"
-                            data-aos-duration="1000"
-                        >
-                            {t('main_box1_description')}
-                        </p>
-                        <NormalButton
-                            button={t('main_box1_button')}
-                            data-aos="fade-up"
-                            data-aos-duration="1000"
-                        />
+                    <div
+                        className="content_box"
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                    >
+                        <p className="title">{t('main_box1_title')}</p>
+                        <p className="description">{t('main_box1_description')}</p>
+                        <NormalButton button={t('main_box1_button')}/>
                     </div>
-                    <div className="image"></div>
+                    <div
+                        className="image"
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                    ></div>
                 </div>
             </div>
             {/* main box 3 */}
             <div className="main_box3">
                 <div className="main_box3_inner">
-                    <div className="image"></div>
-                    <div className="content_box">
-                        <p
-                            className="title"
-                            data-aos="fade-up"
-                            data-aos-duration="1000"
-                        >
-                            {t('main_box2_title')}
-                        </p>
-                        <p
-                            className="description"
-                            data-aos="fade-up"
-                            data-aos-duration="1000"
-                        >
-                            {t('main_box2_description')}
-                        </p>
-                        <NormalButton
-                            button={t('main_box2_button')}
-                            data-aos="fade-up"
-                            data-aos-duration="1000"
-                        />
+                    <div
+                        className="image"
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                    ></div>
+                    <div
+                        className="content_box"
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                    >
+                        <p className="title">{t('main_box2_title')}</p>
+                        <p className="description">{t('main_box2_description')}</p>
+                        <NormalButton button={t('main_box2_button')} />
                     </div>
                 </div>
             </div>
