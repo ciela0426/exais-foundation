fc723ad2103abc81c5eb82e13cf4f59e4339e60d
orbit <ciela0426@naver.com>
2022-10-07 18:45:09 +0900

    [feat] foundation&ecosystem page

---
+++ src/components/exa_foundation/Content3InnerBox.js
@@ -0,0 +1,19 @@
+const Content3InnerBox = ({description, name, roll}) => {
+    return (
+        <div
+            className="content3_inner_box"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+        >
+            <div className="img">
+                <div className="img_inner">
+                    <p>{description}</p>
+                </div>
+            </div>
+            <p className="name">{name}</p>
+            <p className="roll">{roll}</p>
+        </div>
+    );
+}
+
+export default Content3InnerBox;
